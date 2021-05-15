00e6b977ca8063a0980b52d6a479b287116cef15
frederic <mark.s@catalyst-students.com>
2021-05-15 13:31:15 +0200

    refactor: clean out c and tf

--- c_tests.py
+++
@@ -1,6 +0,0 @@
-from ctypes import *
-import math
-
-tests = CDLL('./sine.so')
-tests.sine_node.restype = c_float
-print(tests.sine_node(c_float(0.1)))
--- tests.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdint.h>
-
-void hello(float_t i){
-    float_t f = sin(i);
-    printf("this is the value: sin(%f) = %f\n", f, i);
-}
