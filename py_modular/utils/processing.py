d64ff2c74f23afbe33689285f6bc311aa7cee448
frederic <mark.s@catalyst-students.com>
2021-05-16 22:10:13 +0200

    docu: add windowing utils documentation

--- py_modular/utils/processing.py
+++ py_modular/utils/processing.py
@@ -1,6 +1,13 @@
 from math import floor, asin, sin, pi
 
 def window_grains_sin(grains=[]):
+    """Util used to window an array of audio with half a sine wave
+
+    :param grains: An array of audio buffers that contain audio data to be windowed
+    :type grains: array(array)
+    :returns: An array of the same shape as the input, but windowed
+    :rtype: array(array)
+    """
     for grain in grains:
         grain_length = len(grain)
         for i in range(grain_length):
@@ -8,6 +15,13 @@ def window_grains_sin(grains=[]):
     return grains
 
 def window_grains_tri(grains=[]):
+    """Util used to window an array of audio with half a triangle wave
+
+    :param grains: An array of audio buffers that contain audio data to be windowed
+    :type grains: array(array)
+    :returns: An array of the same shape as the input, but windowed
+    :rtype: array(array)
+    """
     for grain in grains:
         grain_length = len(grain)
         for i in range(grain_length):
