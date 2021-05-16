ab3f9fdc9ca358dcd06d4b398c1e2eb4e8514f97
frederic <mark.s@catalyst-students.com>
2021-05-16 21:22:22 +0200

    docu: document debug utils

--- util_nodes/debug_utils.py
+++ util_nodes/debug_utils.py
@@ -3,9 +3,16 @@ import matplotlib.pyplot as plot
 import numpy as np
 
 def graph_node_lin(node, samples):
+    """Used to graph a node's output with matplotlib over a given ammount of samples on a linear basis.
+
+    :param node: A node that should be graphed over time
+    :param samples: Number of samples the node should be graphed for
+    :type samples: int
+    """
     time = []
     samps = []
     for i in range(samples):
+        # TODO: get sample rate from node?
         sample = node.get_sample(i/48000.0)
         samps.append(sample)
         time.append(i)
@@ -18,9 +25,16 @@ def graph_node_lin(node, samples):
     plot.show()
 
 def graph_node_log(node, samples):
+    """Used to graph a node's output with matplotlib over a given ammount of samples on a logarithmic basis.
+
+    :param node: A node that should be graphed over time
+    :param samples: Number of samples the node should be graphed for
+    :type samples: int
+    """
     time = []
     samps = []
     for i in range(samples):
+        # TODO: get sample rate from node?
         sample = node.get_sample(i/48000.0)
         samps.append(sample)
         time.append(i)
@@ -34,10 +48,17 @@ def graph_node_log(node, samples):
     plot.show()
 
 def graph_transport(tp, samples):
+    """Used to graph a transport over a given number of samples. Usefull for when events need to be seen visually
+
+    :param tp: Transport that should be graphed
+    :param samples: Number of samples the transport should be graphed for
+    :type samples: int
+    """
     samps = []
     time = []
     for i in range(samples):
         time.append(i)
+    # TODO: get block size from transport
     for i in range(int(samples / 1024)):
         b = tp.get_block()
         for x in range(len(b)):
@@ -52,13 +73,29 @@ def graph_transport(tp, samples):
     plot.show()
 
 def record_file(node, filename='recorded_wave.wav', seconds=5):
+    """
+    :param node: Node to be recorded
+    :param filename: Name of the output file
+    :type filename: string
+    :param seconds: Number of seconds to record for
+    :type seconds: int
+    """
     buffer = []
+    # TODO: get samplerate from node? 
     for i in range(48000 * seconds):
         samp = node.get_sample(i / 48000.0) / 10.0
         buffer.append(samp)
     sf.write(filename, buffer, 48000)
 
-def record_transport(tp, filename='recorded_session.wav', seconds=5.0):
+def record_transport(tp, filename='recorded_session.wav', seconds=5):
+    """
+    :param tp: Transport to be recorded
+    :param filename: Name of the output file
+    :type filename: string
+    :param seconds: Number of seconds to record for
+    :type seconds: int
+    """
+    # TODO: is this even working?
     buffer = [[],[]]
     blocks = int(seconds * 48000 / 1024)
     for i in range(blocks):
