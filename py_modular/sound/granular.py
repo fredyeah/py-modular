7a0ce03f5158360d0a6f8692c9fedeaadc435f4a
frederic <mark.s@catalyst-students.com>
2021-05-16 22:18:42 +0200

    docu: add granular init docu

--- py_modular/sound/granular.py
+++ py_modular/sound/granular.py
@@ -1,7 +1,31 @@
 from random import randint, random
 
 class GranBase:
-    def __init__(self, grains, freq=10.0, jitter=0, random_offset=0, grain_range=[None, None], gain=1.0, offset=0.0, gainct=None):
+    """
+    :param grains: An array of audio buffers that will be used as a grain cloud
+    :type grains: array(array)
+    :param freq: Frequency in Hz that grains will be fired at
+    :type freq: float
+    :param jitter: Ammount of jitter to apply to the grain firing frequency
+    :type jitter: float
+    :param random_offset: An ammount of randomness that determines the sequence in which grains are fired
+    :type random_offset: int
+    :param gain: An ammount of gain to apply to the output
+    :type gain: float
+    :param offset: An ammount of offset to apply to the output
+    :type offset: float
+    :ivar freq_in_seconds: The frequency represented as an ammount of time in seconds
+    :vartype freq_in_seconds: float
+    :ivar current_grain: The index of the currently playing grain
+    :vartype current_grain: int
+    :ivar playing_grains: An array of grain indecies that correspond to the grains currently playing
+    :vartype playing_grains: array
+    :ivar grain_positions: An array of indecies that correspond to the playback posistion of each grain
+    :vartype grain_positions: array(array)
+    :ivar time: The last value of time recieved
+    :vartype time: float 
+    """
+    def __init__(self, grains, freq=10.0, jitter=0, random_offset=0, grain_range=[None, None], gain=1.0, offset=0.0):
         self.grains = grains
         self.num_grains = len(grains)
         self.current_grain = 0
@@ -14,8 +38,6 @@ class GranBase:
         self.time = 0.0
         self.gain = float(gain)
         self.offset = float(offset)
-        self.gainct = gainct
-        self.phi = 0.0
     def gain_to(self, gain):
         self.gain = float(gain)
     def make_jitter(self):
@@ -33,17 +55,15 @@ class GranBase:
             if(self.grain_positions[grain] >= len(self.grains[grain])):
                 self.playing_grains.remove(grain)
                 self.grain_positions[grain] = 0
-                self.fire_grain()
+                # self.fire_grain()
             values.append(self.grains[grain][self.grain_positions[grain]])
         if(len(values) > 0):
             return sum(values) / len(values)
         return 0.0
     def get_sample(self, time):
         # self.make_jitter()
-        if not self.gainct == None:
-            self.gain_to(self.gainct.get_sample(time))
         if self.time > (time % self.freq_in_seconds):
-            # self.fire_grain()
+            self.fire_grain()
             pass
         self.time = time % self.freq_in_seconds
         return self.get_pcm_value(time) * self.gain + self.offset
