16bf8bf5a49135e6d9a95254d41e132abad52fb5
frederic <mark.s@catalyst-students.com>
2021-05-17 09:39:24 +0200

    fix: mock imports for docu

--- doc/conf.py
+++ doc/conf.py
@@ -33,6 +33,8 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
+autodoc_mock_imports = ["sounddevice, soundfile"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
