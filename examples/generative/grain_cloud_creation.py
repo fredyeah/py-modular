c3dbb4601264c284027c0a4ce16736473212b889
frederic <mark.s@catalyst-students.com>
2021-05-18 16:42:13 +0200

    feat: add and test grain cloud generation example

--- examples/generative/grain_cloud_creation.py
+++ examples/generative/grain_cloud_creation.py
@@ -1,10 +1,12 @@
 import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 
-from py_modular.generative.utils import generate_grains, create_grains, interpolate_grains
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-grains = create_grains(file_name='some_wav_file.wav', file_location='', number_of_grains=2)
-grains = encode_grains(grains, 'model.ckpt-200000')
+from py_modular.generative.utils import generate_grains, create_grains, interpolate_grains, save_grain_cloud
+
+grains = create_grains(file_name='debussy_printemp.wav', file_location='', number_of_grains=2, checkpoint='wavenet-ckpt/model.ckpt-200000')
+# grains = encode_grains(grains, 'model.ckpt-200000')     http://download.magenta.tensorflow.org/models/nsynth/wavenet-ckpt/model.ckpt-200000.tar
 grains = interpolate_grains(grains, number_of_grains=128)
-grains = generate_grains(grains, 'model.ckpt-200000')
+grains = generate_grains(grains, 'wavenet-ckpt/model.ckpt-200000')
 save_grain_cloud(grains)
