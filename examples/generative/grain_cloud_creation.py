06f4c2e763fec0d5290cc55c58defad15a1b9f37
frederic <mark.s@catalyst-students.com>
2021-05-18 18:30:20 +0200

    feat: adjust grain cloud offsets

--- examples/generative/grain_cloud_creation.py
+++ examples/generative/grain_cloud_creation.py
@@ -5,7 +5,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from py_modular.generative.utils import generate_grains, create_grains, interpolate_grains, save_grain_cloud
 
-grains = create_grains(file_name='debussy_printemp.wav', file_location='', number_of_grains=2, checkpoint='wavenet-ckpt/model.ckpt-200000')
+grains = create_grains(file_name='debussy_printemp.wav', file_location='', grain_size=4096, number_of_grains=2, grain_offset=1680000, grain_space=160000, checkpoint='wavenet-ckpt/model.ckpt-200000')
 grains = interpolate_grains(grains, number_of_grains=128)
 grains = generate_grains(grains, 'wavenet-ckpt/model.ckpt-200000')
 save_grain_cloud(grains)
